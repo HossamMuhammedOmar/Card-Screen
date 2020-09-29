8d68c5bfb076dd28d093649b698b6c892aec0400
HossamMuhammedOmar <hossamomar9876@gmail.com>
2020-09-29 19:07:49 +0200

    finish column and row widgets

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,32 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              color: Colors.white,
+              child: Text('Container 1'),
+            ),
+            SizedBox(width: 50),
+            Container(
+              child: Text('Container 2'),
+              color: Colors.red[400],
+              padding: EdgeInsets.all(20),
+            ),
+            Container(
+              child: Text('Container 3'),
+              padding: EdgeInsets.all(20),
+              color: Colors.yellow[600],
+            ),
+          ],
+        )),
+      ),
+    );
   }
 }
